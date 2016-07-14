c485fd1184c849888af4f7489338ea2bd6b20792
Robert Boone <robert@rlb3.com>
2016-07-14 08:12:32 -0500

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule AzurePushClient.Mixfile do
 
   def project do
     [app: :azure_push_client,
-     version: "0.0.2",
+     version: "0.0.3",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
